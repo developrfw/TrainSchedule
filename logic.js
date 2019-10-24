12f91e4cefd98022703e394f5bba1a5597b1de7c
developrfw <developrfw@gmail.com>
2019-10-23 21:35:33 -0600

    fixed html append and cdn link

--- logic.js
+++ logic.js
@@ -1,12 +1,21 @@
-var firebaseConfig = {
+var config = {
+
+  // apiKey: "AIzaSyBN_MI68X7mcbU2BEwR4y2XkHA4Cc-feFQ",
+  // authDomain: "train-time-b5010.firebaseapp.com",
+  // databaseURL: "https://train-time-b5010.firebaseio.com",
+  // projectId: "train-time-b5010",
+  // storageBucket: "",
+  // messagingSenderId: "335199564551",
+  // appId: "1:335199564551:web:e56373cac5528a13"
+  // // https://train-time-b5010.appspot.com
 
   apiKey: "AIzaSyBN_MI68X7mcbU2BEwR4y2XkHA4Cc-feFQ",
-  authDomain: "train-time-b5010.firebaseapp.com",
-  databaseURL: "https://train-time-b5010.firebaseio.com",
-  projectId: "train-time-b5010",
-  storageBucket: "https://train-time-b5010.appspot.com",
-  messagingSenderId: "335199564551",
-  appId: "1:335199564551:web:e56373cac5528a13"
+    authDomain: "train-time-b5010.firebaseapp.com",
+    databaseURL: "https://train-time-b5010.firebaseio.com",
+    projectId: "train-time-b5010",
+    storageBucket: "train-time-b5010.appspot.com",
+    messagingSenderId: "335199564551",
+    appId: "1:335199564551:web:e56373cac5528a13"
 
 };
 
@@ -16,8 +25,9 @@ firebase.initializeApp(config);
 var trainData = firebase.database();
 
 
-$("add-train-btn").on("click", function (event) {
+$("#add-train-btn").on("click", function (event) {
   event.preventDefault();
+  
 
   // Grabbing the user input in the below code
   var trainName = $("#train-name-input").val().trim();
@@ -36,7 +46,7 @@ $("add-train-btn").on("click", function (event) {
 
   };
 
-
+  console.log(newTrain);
   // Uploading train data to the database
   trainData.ref().push(newTrain);
 
@@ -52,6 +62,7 @@ $("add-train-btn").on("click", function (event) {
 
 // Creating a Firebase event for adding trains to the data base as well as creating a row in the HTML when the user makes a new entry
 trainData.ref().on("child_added", function (childSnapshot, prevChildKey) {
+  
   console.log(childSnapshot.val());
 
   // Storing everything in a variable
@@ -60,40 +71,44 @@ trainData.ref().on("child_added", function (childSnapshot, prevChildKey) {
   var tFrequency = childSnapshot.val().frequency;
   var tFirstTrain = childSnapshot.val().firstTrain;
 
-  var timeArr = tFirstTrain.split(":");
-  var trainTime = moment()
-    .hours(timeArr[0])
-    .minutes(timeArr[1]);
-  var maxMoment = moment.max(moment(), trainTime);
-  var tMinutes;
-  var tArrival;
+    if (tFirstTrain){
+      
+      var timeArr = tFirstTrain ? tFirstTrain.split(":") : null;
+      var trainTime = moment()
+        .hours(timeArr[0])
+        .minutes(timeArr[1]);
+      var maxMoment = moment.max(moment(), trainTime);
+      var tMinutes;
+      var tArrival;
 
-  // If first train is later than the current time, set arrival to the first train time
+      // If first train is later than the current time, set arrival to the first train time
 
-  if (maxMoment === trainTime) {
-    tArrival = trainTime.format("hh:mm A");
-    tMinutes = trainTime.diff(moment(), "minutes");
+      if (maxMoment === trainTime) {
+        tArrival = trainTime.format("hh:mm A");
+        tMinutes = trainTime.diff(moment(), "minutes");
 
-  } else {
-    var differenceTimes = moment().diff(trainTime, "minutes");
-    var tRemainder = differenceTimes % tFrequency;
-    tMinutes = tFrequency - tRemainder;
+      } else {
+        var differenceTimes = moment().diff(trainTime, "minutes");
+        var tRemainder = differenceTimes % tFrequency;
+        tMinutes = tFrequency - tRemainder;
 
-    tArrival = moment()
-      .add(tMinutes, "m")
-      .format("hh:mm A");
-  }
-  console.log("tMinutes:", tMinutes);
-  console.log("tArrival:", tArrival);
+        tArrival = moment()
+          .add(tMinutes, "m")
+          .format("hh:mm A");
+      }
+      console.log("tMinutes:", tMinutes);
+      console.log("tArrival:", tArrival);
 
-  // Add each train's data into the table
-  $("#train-table > tbody").append(
-    $("<tr>").append(
-    $("<tr>").text(tName),
-    $("<td>").text(tDestination),
-    $("<td>").text(tFrequency),
-    $("<td>").text(tArrival),
-    $("<td>").text(tMinutes)
-   )
-  );
+      // Add each train's data into the table
+      $("#train-table > tbody").append(
+        $("<tr>").append(
+        $("<td>").text(tName),
+        $("<td>").text(tDestination),
+        $("<td>").text(tFrequency),
+        $("<td>").text(tArrival),
+        $("<td>").text(tMinutes)
+      )
+      );
+    }
+  
 });
